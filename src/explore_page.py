5bfb21859131351f1c619e0a008e79194a457107
Aiman Rosli <aimanrosli977@yahoo.com>
2024-02-02 00:28:24 +0800

    Added scikitlearn
    Signed-off-by: Aiman Rosli <aimanrosli977@yahoo.com>

--- src/explore_page.py
+++ src/explore_page.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import streamlit.components.v1 as components
 import seaborn as sns
+import sklearn
 #import pygwalker as pyg
 
 def addValue(row):
